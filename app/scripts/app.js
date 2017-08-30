51a271adfd79e0b8b1b1f9f0a6b41253e2682fe0
jxinchao <425775961@qq.com>
2017-08-30 17:07:32 +0800

    路由配置

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,10 +13,22 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
       url:'/index',
       templateUrl: 'views/main.html',
       controller:'MainCtrl'
+    }).state('index.indexright',{
+      url:'/indexright',
+      templateUrl: 'views/indexright.html'
     }).state('index.yuangong',{
       url:'/yuangong',
       templateUrl: 'views/yuangong.html'
-  });
-  $urlRouterProvider.when('','index');
+    }).state('index.renshi',{
+      url:'/renshi',
+      templateUrl: 'views/renshi.html'
+    }).state('index.recycle',{
+      url:'/recycle',
+      templateUrl: 'views/recycle.html'
+    }).state('index.help',{
+      url:'/help',
+      templateUrl: 'views/help.html'
+    });
+  $urlRouterProvider.when('','/index/indexright');
 });
 
